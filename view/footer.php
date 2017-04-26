4c645099de9dbc40bce8a0bad25f89b3b3a4a69e
Rebecca J Cortes <rjc37@afsconnect2.njit.edu>
2017-04-26 19:02:00 -0400

    view with header and footer

---
+++ view/footer.php
@@ -0,0 +1,7 @@
+<footer class="container-fluid text-center">
+    <p class="copyright" style="color:black;">
+        &copy; <?php echo date("Y"); ?> To-Do, Inc.
+    </p>
+</footer>
+</body>
+</html>
