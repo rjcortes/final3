6bcccafffc028f788f825908b554e2279347858b
Rebecca J Cortes <rjc37@afsconnect2.njit.edu>
2017-04-26 19:18:42 -0400

    remove subscribe form from header

--- view/header.php
+++ view/header.php
@@ -210,11 +210,6 @@
   <h1>To-Do</h1> 
   <p>We specialize in time management</p> 
   <form>
-    <div class="input-group">
-      <input type="email" class="form-control" size="50" placeholder="Email Address" required>
-      <div class="input-group-btn">
-        <button type="button" class="btn btn-danger">Subscribe</button>
-      </div>
     </div>
   </form>
 </div>
