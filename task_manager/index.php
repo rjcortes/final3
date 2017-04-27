010ad81ecb4a84e02b8ca344ed50099e02248e91
Rebecca J Cortes <rjc37@afsconnect2.njit.edu>
2017-04-27 11:47:58 -0400

    adjust table column widths

--- task_manager/index.php
+++ task_manager/index.php
@@ -108,33 +108,32 @@
       <div class="col-sm-8">
         <h1 style="color:black">Welcome
             <?php
-            $servername = "sql2.njit.edu";
-            $username = "rjc37";
-            $password = "cosette2";
-            $dbname = "rjc37";
+              $servername = "sql2.njit.edu";
+              $username = "rjc37";
+              $password = "cosette2";
+              $dbname = "rjc37";
 
-            // Create connection
-            $conn = new mysqli($servername, $username, $password, $dbname);
-            // Check connection
-            if ($conn->connect_error) {
-              die("Connection failed: " . $conn->connect_error);
-            } 
+              // Create connection
+              $conn = new mysqli($servername, $username, $password, $dbname);
+              // Check connection
+              if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+              } 
 
-            $sql = "SELECT * FROM accounts";
-            $result = $conn->query($sql);
+              $sql = "SELECT * FROM accounts";
+              $result = $conn->query($sql);
 
-            if ($result->num_rows > 0) {
+              if ($result->num_rows > 0) {
 
-            while($row = $result->fetch_assoc()) {
-              echo $row["fname"]. " ". $row["lname"];
-            }
-            } else {
-              echo "0 results";
-            }
+              while($row = $result->fetch_assoc()) {
+                echo $row["fname"]. " ". $row["lname"];
+              }
+              } else {
+                echo "0 results";
+              }
 
-            $conn->close();
+              $conn->close();
             ?>
-            !
         </h1>
       </div>
     </div>
@@ -143,54 +142,54 @@
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
   <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
 
-  <div class="container">
+<div class="container">
     <div class="row">
       <div class="col-md-12">
         <h4>My Tasks</h4>
           <div class="table-responsive">
             <table id="mytable" class="table table-bordred table-striped" style="color:black;">
                 <thead>
-                  <th><input type="checkbox" id="checkall" /></th>
-                  <th>Task</th>
-                  <th>Due Date</th>
-                  <th>Edit</th>
-                  <th>Delete</th>
+                  <th style="width:5%"><input type="checkbox" id="checkall" /></th>
+                  <th style="width:65%">Task</th>
+                  <th style="width:10%">Due Date</th>
+                  <th style="width:10%">Edit</th>
+                  <th style="width:10%">Delete</th>
                 </thead>
                 <tbody>
                   <tr>
                     <td><input type="checkbox"  class="checkbox" name="packersOff" value="1" /></td>
                     <td><?php echo $task['task']; ?></td>
                     <td><?php echo $duedate['duedate']; ?></td>
-                    <td><form action="update_product"><input type="submit" value="edit"/></td>
-                    <td><form action="delete_product"><input type="submit" value="delete"/></td>
+                    <td><input type="submit" class="btn btn-danger" value="edit"/></td>
+                    <td><input type="submit" class="btn btn-danger" value="delete"/></td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"  class="checkbox" name="packersOff" value="1" /></td>
                     <td><?php echo $task['task']; ?></td>
                     <td><?php echo $duedate['duedate']; ?></td>
-                    <td><form action="update_product"><input type="submit" value="edit"/></td>
-                    <td><form action="delete_product"><input type="submit" value="delete"/></td>
+                    <td><input type="submit" class="btn btn-danger" value="edit"/></td>
+                    <td><input type="submit" class="btn btn-danger" value="delete"/></td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"  class="checkbox" name="packersOff" value="1" /></td>
                     <td><?php echo $task['task']; ?></td>
                     <td><?php echo $duedate['duedate']; ?></td>
-                    <td><form action="update_product"><input type="submit" value="edit"/></td>
-                    <td><form action="delete_product"><input type="submit" value="delete"/></td>
+                    <td><input type="submit" class="btn btn-danger" value="edit"/></td>
+                    <td><input type="submit" class="btn btn-danger" value="delete"/></td>
                   </tr>
                   <tr>
                     <td><input type="checkbox"  class="checkbox" name="packersOff" value="1" /></td>
                     <td><?php echo $task['task']; ?></td>
                     <td><?php echo $duedate['duedate']; ?></td>
-                    <td><form action="update_product"><input type="submit" value="edit"/></td>
-                    <td><form action="delete_product"><input type="submit" value="delete"/></td>
+                    <td><input type="submit" class="btn btn-danger" value="edit" action="" /></td>
+                    <td><input type="submit" class="btn btn-danger" value="delete" action="addtask.php" /></td>
                   </tr>
                 </tbody>
             </table>
-            <form method="post" class="add-new-task" action="add_task" autocomplete="off">
+            <form method="post" action="addtask.php" autocomplete="off">
               <div class="input-group">
                 <div class="input-group-btn">
-                  <input type="submit" name="new-task" value="Add New Task" class="btn btn-danger"/>
+                  <input type="submit" value="Add New Task" class="btn btn-danger"/>
                 </div>
               </div>
             </form>
@@ -199,6 +198,221 @@
     </div>
   </div>
 </div>
+
+
+<br><hr><br>
+
+<div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h4>Completed Tasks</h4>
+          <div class="table-responsive">
+            <table id="mytable" class="table table-bordred table-striped" style="color:black;">
+                <thead>
+                  <th style="width:5%"><input type="checkbox" id="checkall" /></th>
+                  <th style="width:65%">Task</th>
+                  <th style="width:20%">Due Date</th>
+                  <th style="width:10%">Delete</th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td><input type="checkbox"  class="checkbox" name="packersOff" value="1" /></td>
+                    <td><?php echo $task['task']; ?></td>
+                    <td><?php echo $duedate['duedate']; ?></td>
+                    <td><input type="submit" class="btn btn-danger" value="delete"/></td>
+                  </tr>
+                  <tr>
+                    <td><input type="checkbox"  class="checkbox" name="packersOff" value="1" /></td>
+                    <td><?php echo $task['task']; ?></td>
+                    <td><?php echo $duedate['duedate']; ?></td>
+                    <td><input type="submit" class="btn btn-danger" value="delete"/></td>
+                  </tr>
+                  <tr>
+                    <td><input type="checkbox"  class="checkbox" name="packersOff" value="1" /></td>
+                    <td><?php echo $task['task']; ?></td>
+                    <td><?php echo $duedate['duedate']; ?></td>
+                    <td><input type="submit" class="btn btn-danger" value="delete"/></td>
+                  </tr>
+                  <tr>
+                    <td><input type="checkbox"  class="checkbox" name="packersOff" value="1" /></td>
+                    <td><?php echo $task['task']; ?></td>
+                    <td><?php echo $duedate['duedate']; ?></td>
+                    <td><input type="submit" class="btn btn-danger" value="delete" action="addtask.php" /></td>
+                  </tr>
+                </tbody>
+            </table>
+            <form method="post" action="addtask.php" autocomplete="off">
+              <div class="input-group">
+                <div class="input-group-btn">
+                  <input type="submit" value="Add New Task" class="btn btn-danger"/>
+                </div>
+              </div>
+            </form>
+          </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<!-- Imports
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-2.2.3.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/4.0.5/handlebars.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"></script>
+
+Application
+<div class="todo-container">
+  <h1>Bootstrap Todo App</h1>
+  <div class="add-todo-container row">
+    <div class="col-xs-10">
+      <input class="form-control" type="text" placeholder="new todo">
+    </div>
+    <div class="col-xs-2">
+      <button class="btn btn-primary">Add</button>
+    </div>
+  </div>
+  <ul class="list-group"></ul>
+</div>
+<div class="too-small"> This site does not support mobile. </div>
+
+<style>
+  .todo-container {
+  width: 500px;
+  margin: 5px auto;
+  margin-top: 20px;
+  }
+  .add-todo-container {
+    margin-bottom: 10px;
+  }
+  .add-todo-container .col-md-10 {
+    padding: 0;
+  }
+  .add-todo-container .col-md-2 {
+    padding-right: 0;
+    text-align: right;
+  }
+
+  .too-small {
+    display: none;
+  }
+
+  @media (max-width: 500px) {
+    .todo-container {  
+      display: none;
+    }
+    .too-small {
+      display: block;
+    }
+  }
+</style>
+
+
+<script type="text/x-template">
+  <li class="list-group-item row {{#if completed}}disabled{{/if}}">
+    <div class="col-xs-1">
+      <input type="checkbox" {{#if completed}}checked{{/if}}>
+    </div>
+    <div class="col-xs-10">{{title}}</div>
+    <div class="col-xs-1">
+      <button type="button" class="close" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  </li>
+</script>
+
+<script>
+  // on document load
+  $(function(){
+  
+  // Data Model
+  var todos = [];
+  
+  // Application
+  var template;
+  var app = {
+    init: function(){
+      app.compileTemplates();
+      app.render();
+    },
+    render: function(){
+      // render the todos
+      var todoHtml = _.map(todos, function(todo){
+        return template(todo);
+      });
+      app.unbindEvents();
+      $('ul.list-group').html(todoHtml.join(""));
+      app.bindEvents();
+    },
+    compileTemplates: function(){
+      template = $('[type="text/x-template"]');
+      template = Handlebars.compile(template.first().html());
+    },
+    unbindEvents: function(){
+      $('.list-group-item').off();
+      $('.add-todo-container button').off();
+      $('input[type="checkbox"]').off();
+      $('.list-group-item button').off();
+    },
+    bindEvents: function(){
+      app.bindHoverEvents();
+      app.bindCheckboxEvents();
+      app.bindAddTodoEvents();
+      app.bindRemoveTodoEvents();
+    },
+    bindHoverEvents: function(){
+      var $items = $('.list-group-item');
+      $items.on('mouseover', function(event){
+        $(this).addClass('list-group-item-success');
+      });
+      $items.on('mouseout', function(){
+        $(this).removeClass('list-group-item-success');
+      });
+    },
+    bindCheckboxEvents: function(){
+      var $checkboxes = $('input[type="checkbox"]');
+      $checkboxes.on('change', function(){
+        var wasChecked = $(this).is(':checked');
+        if (!wasChecked) {
+          $(this).parent().parent().removeClass("disabled");
+        } else {
+          $(this).parent().parent().addClass("disabled");
+        }
+      });
+    },
+    bindAddTodoEvents: function(){
+      var $container = $('.add-todo-container');
+      $container.find('button').on('click', function(){
+        var newTodoTitle = $container.find('input').val();
+        if (_.isString(newTodoTitle) && newTodoTitle.length > 2) {
+          var newTodoObject = { title: newTodoTitle, completed: false };
+          todos.push(newTodoObject);
+          $container.find('input').val("");
+          app.render(); 
+        }
+      });
+    },
+    bindRemoveTodoEvents: function(){
+      $('.list-group-item button').on('click', function(){
+        var index = $(this).parent().parent().index();
+        todos.splice(index, 1);
+        app.render();
+      });
+    }
+  };
+  
+  app.init();
+  
+  });
+</script>  -->
 
 
 <?php include '../view/footer.php'; ?>
