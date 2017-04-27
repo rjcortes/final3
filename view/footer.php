b1b5ef4af2ec637909e896291c8f6bbe3d7b67bb
Rebecca J Cortes <rjc37@afsconnect2.njit.edu>
2017-04-27 13:55:18 -0400

    footer updated

--- view/footer.php
+++ view/footer.php
@@ -1,5 +1,6 @@
 <footer class="container-fluid text-center">
     <p class="copyright" style="color:black;">
+    	<a href="../logout.php">Log Out</a><br>
         &copy; <?php echo date("Y"); ?> To-Do, Inc.
     </p>
 </footer>
